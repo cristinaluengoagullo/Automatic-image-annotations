d35293bc418a69d974e062abd0abb29f4efa7b16
Cristina Luengo <cristinaluengoagullo@gmail.com>
2014-12-02 01:09:48 +0100

    Header changes so it has all the attributes

--- mat2arff.m
+++ mat2arff.m
@@ -9,16 +9,18 @@ function write_header(newFile,set_type)
     dlmwrite(newFile,strcat({'@relation '},set_type),'delimiter','');
     % Features columns
     % 1:3) RCG components
-    % 4) Texture component
+    % 4:7) Texture components
     % 5) Region of the size each pixel from the dataset belongs to
     % 6:7) Reative position of the region each pixel belongs to 
     dlmwrite(newFile,'@attribute R REAL','delimiter','','-append');
     dlmwrite(newFile,'@attribute G REAL','delimiter','','-append');
     dlmwrite(newFile,'@attribute B REAL','delimiter','','-append');
-%     dlmwrite(newFile,'@attribute text REAL','delimiter','','-append');
-%     dlmwrite(newFile,'@attribute region_size REAL','delimiter','','-append');
-%     dlmwrite(newFile,'@attribute x REAL','delimiter','','-append');
-%     dlmwrite(newFile,'@attribute y REAL','delimiter','','-append');
+    dlmwrite(newFile,'@attribute text1 REAL','delimiter','','-append');
+    dlmwrite(newFile,'@attribute text2 REAL','delimiter','','-append');
+    dlmwrite(newFile,'@attribute text3 REAL','delimiter','','-append');
+    dlmwrite(newFile,'@attribute region_size REAL','delimiter','','-append');
+    dlmwrite(newFile,'@attribute x_region REAL','delimiter','','-append');
+    dlmwrite(newFile,'@attribute y_region REAL','delimiter','','-append');
     dlmwrite(newFile,'@attribute type {1,2,3,4,5,6,7}','delimiter','','-append');
     dlmwrite(newFile,'@data','delimiter','','-append');
 end
